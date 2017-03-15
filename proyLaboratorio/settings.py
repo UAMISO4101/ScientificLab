5e9d0a09549222c550eb9eace62cf1e23ff5a71b
flxruizruiz <fe.ruiz@uniandes.edu.co>
2017-03-14 23:49:03 -0500

    correccion setting

--- proyLaboratorio/settings.py
+++ proyLaboratorio/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-REST_FRAMEWORK = {'EXCEPTION_HANDLER':'rest_framework.views.exception_handler'}
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'laboratorio',
     'bootstrap3',
-    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -83,10 +80,10 @@ WSGI_APPLICATION = 'proyLaboratorio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Laboratory',
-        'USER': 'sid',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'NAME': 'dcf0khq7fkhc7g',
+        'USER': 'elehobzpyludfo',
+        'PASSWORD': '5edd0781564ba20a7928f159c2b02ffa75dfdfcd5709dc15a888ed7024ba798f',
+        'HOST': 'ec2-23-21-46-94.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -135,9 +132,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_URL = '/images/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'images'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
