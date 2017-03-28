d92cdbd4f3e939866202d520bc7d986828500853
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-03-27 23:22:19 -0500

    Delete button "delete" in protocols' template

--- laboratorio/static/js/Laboratory/ProtocoloController.js
+++ laboratorio/static/js/Laboratory/ProtocoloController.js
@@ -16,27 +16,19 @@ function listarProtocolos(){
     "headers": {}
     }
     var html="";
-     //alert(host+url)
-     $.ajax(settings).done(function (response) {
-        //alert("Responde Ajax "+response);
-       for( i in response) {
-            var objeto = response[i] ;
-            console.log(objeto.titulo+" * "+objeto.descripcion);
 
+     $.ajax(settings).done(function (response) {
+
+       for(var i in response) {
+            var objeto = response[i] ;
             html+="<tr class='alt'>";
             html+="<td><input type='checkbox'/></td>";
             html+="<td>"+objeto.titulo+"</td>";
             html+="<td>"+objeto.descripcion+"</td>";
             html+="<td>"+objeto.version+"</td>";
             html+="<td>"+objeto.categoria+"</td>";
-            html+="<td><a href='url+'laboratorio:editarProyecto' class='btn btn-info btn-round'><span class='glyphicon glyphicon-pencil'></span></a></td>";
+           // html+="<td><a href='url+'laboratorio:editarProyecto' class='btn btn-info btn-round'><span class='glyphicon glyphicon-pencil'></span></a></td>";
             html+="</tr>";
-
-
-//       console.log(objeto.fields.titulo);  // (o el campo que necesites)
-        //html+=objeto.fields.titulo ;
-           // console.log(response);
-   //data.artists.items[i].href
        }
        $("#myTable tbody").html(html);
         //console.log(response);
