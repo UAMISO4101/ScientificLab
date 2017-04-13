63084abddb9f4b2e6118feb6c33c770ef96a68e5
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-04-12 19:14:16 -0500

    first commit branch

--- laboratorio/tests/frontEnd/testViewProjectProgress.py
+++ laboratorio/tests/frontEnd/testViewProjectProgress.py
@@ -6,8 +6,8 @@ class testViewProjectProgress(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.baseUrl = self.live_server_url + '/laboratorio/';
-        self.nombreUsuario = 'usuarioprueba';
-        self.nombre = 'Usuario Prueba';
+        self.nombreUsuario = 'testUser';
+        self.nombre = 'Test User';
         self.password = 'agiles123';
 
     def tearDown(self):
