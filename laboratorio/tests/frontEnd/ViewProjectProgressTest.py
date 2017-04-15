3abfcf0cff8c5105602eb2a57561c200dff77009
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-04-14 20:47:49 -0500

    Step 3. feature add progress(RED)

--- laboratorio/tests/frontEnd/ViewProjectProgressTest.py
+++ laboratorio/tests/frontEnd/ViewProjectProgressTest.py
@@ -15,10 +15,8 @@ class ViewProjectProgressTest(TestCase):
         self.browser.get('http://localhost:8000/laboratorio')
         self.do_login()
         self.go_to_project_progress()
-        self.assertEqual('Laboratorio Uniandes', self.browser.title)
-        btnAddProgress = self.browser.find_element_by_id('btnAddProgress')
-        lblTotalReportedProgress = self.browser.find_element_by_id('counterProgress')
-        totalReportedProgress = lblTotalReportedProgress.get_attribute("value")
+        self.add_progress()
+
 
 
     def go_to_project_progress(self):
@@ -28,6 +26,7 @@ class ViewProjectProgressTest(TestCase):
         linkReport = self.browser.find_element_by_id('report_1')
         linkReport.click()
         time.sleep(2)
+        self.assertEqual('Laboratorio Uniandes', self.browser.title)
 
     def do_login(self):
         link = self.browser.find_element_by_id('link_iniciar_sesion')
@@ -43,3 +42,25 @@ class ViewProjectProgressTest(TestCase):
         btn = self.browser.find_element_by_id('btn_iniciar_sesion')
         btn.click()
         time.sleep(3)
+
+    def add_progress(self):
+        totalReportedProgressBeforeSave = self.browser.find_element_by_id('counterProgress').get_attribute("value")
+
+        btnAddProgress = self.browser.find_element_by_id('btnAddProgress')
+        btnAddProgress.click()
+
+        progress = self.browser.find_element_by_id('projectProgress')
+        progress.send_keys('20')
+
+        comment = self.browser.find_element_by_id('comment')
+        comment.send_keys('avance del proyecto')
+
+        btnSave = self.browser.find_element_by_id('btnSave')
+        btnSave.click()
+
+        btnReturn = self.browser.find_element_by_id('btnReturn')
+        btnReturn.click()
+
+        totalReportedProgressAfterSave = self.browser.find_element_by_id('counterProgress').get_attribute("value")
+
+        self.assertEqual(totalReportedProgressAfterSave, totalReportedProgressBeforeSave+1)
