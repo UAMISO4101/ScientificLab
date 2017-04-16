b8887c4b8ff7f3427e757fb5f981516542e1bf83
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-04-15 20:34:43 -0500

    Show projects name

--- laboratorio/static/js/Laboratory/ReportController.js
+++ laboratorio/static/js/Laboratory/ReportController.js
@@ -1,3 +1,13 @@
-/**
- * Created by paula on 4/15/2017.
- */
+function showProyectsNames(response){
+    var nameProyectList =$("#projects");
+    var project;
+    nameProyectList.append(new Option("Seleccione un proyecto", -1));
+    for (var i=0; i <response.length; i++){
+        project = response[i];
+        nameProyectList.append(new Option(project.fields.nombre, project.pk));
+    }
+ }
+
+ function paintChart() {
+     var idProject= $('#projects option:selected').val();
+ }
