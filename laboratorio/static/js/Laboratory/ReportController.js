25a93b57c738248aa1c968dd1e0580acbfe80ae5
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-04-15 21:22:13 -0500

    Showing progress table

--- laboratorio/static/js/Laboratory/ReportController.js
+++ laboratorio/static/js/Laboratory/ReportController.js
@@ -8,6 +8,44 @@ function showProyectsNames(response){
     }
  }
 
- function paintChart() {
+ function paintChart(urlListProgress) {
      var idProject= $('#projects option:selected').val();
+     if(idProject === undefined || idProject == -1) {
+         alertify.error("Seleccione un proyecto para poder generar la gráfica de avance.");
+         return;
+     }
+     urlListProgress +="?id="+idProject;
+     listProgress(urlListProgress);
  }
+
+ function listProgress(urlListProgress){
+
+     $.ajax({
+        url:host+urlListProgress,
+        method:"GET",
+        success:function(response){paintTableProgress(response);},
+        error:errorGetProgress,
+        async:true,
+        crossDomain:true
+    });
+}
+
+function errorGetProgress(response) {
+    alertify.error("Error al obtener información del proyecto");
+}
+
+function  paintTableProgress(data) {
+    $('#listProgress').removeAttr("hidden");
+
+    var table = $('#listReportedProgress');
+    table.DataTable().destroy();
+    table.DataTable({
+        searching:false,
+        data: data,
+        columns: [
+            { data: "fecha" },
+            { data: "reporte" },
+            { data: "comentario" }
+        ]
+    });
+}
